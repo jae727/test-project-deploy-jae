2aabbce83696eca95c2cbf856d6ee065303ce447
Jae Kim <jae.kim@deem.com>
2014-01-17 11:28:26 -0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProjectDeployJae::Application.config.secret_key_base = 'bb4b463352b2e705c0926a049e458b40c6226cf68e8a5f31e483831fbff7d411705f4746217aa3ad218d073450c266df96c6486d033490de41c065c29be0ee45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestProjectDeployJae::Application.config.session_store :cookie_store, key: '_test-project-deploy-jae_session'
